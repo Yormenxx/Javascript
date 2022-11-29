4b0a2724651a90b62010c419aeddb9127f2a281c
Reaper Diper <105932267+Yormenxx@users.noreply.github.com>
2022-11-29 09:43:40 -0500

    Update index.js

--- DOM/ESTILOS-CLASES/index.js
+++ DOM/ESTILOS-CLASES/index.js
@@ -1,3 +1,8 @@
+//Importante tener en cuenta que con la propiedad style del elemento no podremos acceder 
+//a propiedades declaradas en una hoja de estilos externa al documento HTML, solamente podremos acceder a los 
+//estilos declarados dentro del atributo style del elemento, si queremos consultar propiedades establecidas desde 
+//la hoja de estilos externa debemos usar getComputedStyle
+
 
 // PARA ACCEDER A STYLE Y MODIFICAR ESTILOS EN LINEAS NO ES
 //BUENO HACERLO CON LA NOTACION DEL PUNTO
@@ -11,7 +16,7 @@ console.log($linkDom.style);
 console.log($linkDom.style.backgroundColor);
 console.log($linkDom.Style.color)
 //CUANDO ACCEDEMOS CON LAS COMPUTED STYLES NOS MUESTRA LOS VALORES QUE EL
-//CSS APLICA AL NAVEGADOR
+//CSS APLICA AL NAVEGADOR 
 console.log(window.getComputedStyle($linkDom))
 
 console.log(getComputedStyle($linkDom).getPropertyValue("color"))
@@ -34,10 +39,22 @@ console.log($linkDom.style);
 //VARAIBLES CSS --CUSTOM PROPERTIES
 
 
+const $html = document.documentElement,
+$body = document.body;
 
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color")
 
+let varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color")
 
+console.log(varDakColor, varYellowColor)
 
+$body.style.backgroundColor= varDarkColor;
 
+$body.style.color = varYellowColor;
 
+//PARA ACCEDER A UN VALOR DE LAS COMPUTED STYLES
+
+getComputedStyle($linkDom).getPropertyValue("backgroundColor")
+
+$linkDom.setProperty("color","white");
 
