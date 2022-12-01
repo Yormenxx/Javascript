5ce2b134638d302d731a112cf08038f1ed62b414
Reaper Diper <105932267+Yormenxx@users.noreply.github.com>
2022-12-01 09:42:35 -0500

    Create inde.js

---
+++ DOM/TEXTO/inde.js
@@ -0,0 +1,15 @@
+//INNERTEXT Y TEXTCONTET SIRVEN PARA AGREGAR TEXTO
+
+const $whatisDom = document.getElementById("que-es")
+
+let text = `<p>soy un parrafo con muchas etiquetas <i>lol</i> </p>`
+
+$whatisDom.innerText = text;
+
+innerText = agrega contenido sin el estandar
+
+textContent = agrega contenido con el estandar
+
+innnerHTML = renderiza el codigo en HTML o sea si muestra las etiquetas con conteido original
+
+
