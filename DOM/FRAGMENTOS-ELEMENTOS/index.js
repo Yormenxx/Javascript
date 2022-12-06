cb60a9c990a1f79ca31564e6eea547ebebf20cb9
Reaper Diper <105932267+Yormenxx@users.noreply.github.com>
2022-12-06 07:55:20 -0500

    Update index.js

--- DOM/FRAGMENTOS-ELEMENTOS/index.js
+++ DOM/FRAGMENTOS-ELEMENTOS/index.js
@@ -1,1 +1,29 @@
 
+//VAMOS A EMPEZAR A CREAR NUESTRAS ETIQUETAS HTML
+
+//EL DOM TIENE UNA PROPIEDAD QUE NOS PERMITE CREAR ELEMENTOS DINAMICAMENTE 
+
+const $figure = document.createElment("figure"),
+      $img = document.createElement("img"),
+      $figcaption = document.createElement("figcaption"),
+      $figcaptionText= document.createTextNode("Animals"),
+      $cards = document.querySelector("$cards")
+      
+//CON APPENDCHILD NOS PERMITE AGREGAR UN NODO AL ELEMENTO
+
+$img.setAttribute("src","https://placeimg.com/200/200/animals")
+$img.setAttribute("ALT", "Animals")
+
+$cards.classList.add("card")
+
+$figcaption.appendChild($figcaptionText)
+$figure.appendChild($img)
+$figure.appendChild($figcaption)
+$cards.appendChild($figure)
+
+
+
+
+
+
+
